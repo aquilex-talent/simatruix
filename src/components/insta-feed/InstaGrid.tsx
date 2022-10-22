6d4a3857553448554fb666c8ccd7a0b778de596f
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-10-22 18:13:31 +0100

    Add skeletons and clean up code

--- src/components/insta-feed/InstaGrid.tsx
+++ src/components/insta-feed/InstaGrid.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Skeleton from "@mui/material/Skeleton";
+
 import { InstaItem } from './InstaFeed';
 
 interface InstaGridProps {
@@ -7,8 +9,16 @@ interface InstaGridProps {
 }
 
 const InstaGrid = ({ items }: InstaGridProps) => {
+
+  if (items.length === 0) {
+    return (
+      <div style={{ width: '100%', display: "grid", gridTemplateColumns: "repeat(3,1fr)", gridTemplateRows: "repeat(3,1fr)", rowGap: "2%", columnGap: "2%" }}>
+        {(new Array(9)).fill(<Skeleton variant='rectangular' sx={{ display: 'block', objectFit: 'cover', width: '100%', height: '100%', aspectRatio: "1/ 1" }} />)}
+      </div>
+    )
+  }
   return (
-    <div style={{ width: '100%', display: "grid", gridTemplateColumns: "repeat(3,1fr)", gridTemplateRows: "repeat(3,1fr)", rowGap: "2%", columnGap: "2%"}}>
+    <div style={{ width: '100%', display: "grid", gridTemplateColumns: "repeat(3,1fr)", gridTemplateRows: "repeat(3,1fr)", rowGap: "2%", columnGap: "2%" }}>
       {items.map(item => (
         <img src={item.mediaUrl} style={{ display: 'block', objectFit: 'cover', width: '100%', height: '100%', aspectRatio: "1/ 1" }} />
       ))}
