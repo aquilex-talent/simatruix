e6e45901b5e3d661e3c730e04854b7d20cad678f
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-10-22 16:17:26 +0100

    Clean up code

--- src/components/insta-feed/InstaGrid.tsx
+++ src/components/insta-feed/InstaGrid.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { Grid } from '@mui/material';
+import { Grid, GridSpacing } from '@mui/material';
 import { InstaItem } from './InstaFeed';
 import styled from '@emotion/styled';
+import { ResponsiveStyleValue } from '@mui/system';
 
 interface InstaGridProps {
   items: InstaItem[];
@@ -12,29 +13,40 @@ const StyledImage = styled.img`
 display: block;
 object-fit: cover;
 
-@media (max-width: 599px) {
-  width: 200px;
-  height: 200px;
+@media (max-width: 678px) {
+  width: 180px;
+  height: 180px;
 }
 
-@media (min-width: 600px) and (max-width: 899px) {
-  width: 250px;
-  height: 250px;
+@media (min-width: 679px) and (max-width: 899px) {
+  width: 210px;
+  height: 210px;
 }
 
-@media (min-width: 900px){
-  width: 300px;
-  height: 300px;
+@media (min-width: 900px) and (max-width: 1199px){
+  width: 227px;
+  height: 227px;
+}
+
+@media (min-width: 1200px) and (max-width: 1589px) {
+  width: 230px;
+  height: 230px;
+}
+
+@media (min-width: 1560px) {
+  width: 280px;
+  height: 280px;
 }
 `
 
 const InstaGrid = ({ items }: InstaGridProps) => {
+  const poop: ResponsiveStyleValue<GridSpacing> = { sm: 2, md: 1.57, lg: 1 }
   return (
     <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auto', marginTop: 80 }}>
       <Grid
         container
-        rowSpacing={1}
-        columnSpacing={1}
+        rowSpacing={poop}
+        columnSpacing={poop}
         alignItems="center"
         justifyContent="center"
         paddingBottom={4}
