2d406170ea315c76207aa22b30d56937f4f37b6b
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-10-24 13:41:23 +0100

    Remove nocodeapi instagram dep
    Get the instagram images using instagram basic api

--- src/components/insta-feed/InstaFeed.tsx
+++ src/components/insta-feed/InstaFeed.tsx
@@ -9,39 +9,50 @@ export interface InstaItem {
 const InstaFeed = () => {
   const [instaItems, setInstaItems] = useState<InstaItem[]>([]);
 
-  const instaUrl = "https://v1.nocodeapi.com/ferrouskid/instagram/LnhwHoJFqgJvTbbS";
+  const userId = process.env.REACT_APP_USER_ID;
+  const accessToken = process.env.REACT_APP_ACCESS_CODE;
+
+  const instaUrl = `https://graph.instagram.com/${userId}/media?access_token=${accessToken}`;
 
   useEffect(() => {
+    const fetchMedia = async (id: string) => {
+      const mediaUrl = `https://graph.instagram.com/${id}?access_token=${accessToken}&fields=media_url,permalink`;
+
+      const res = await fetch(mediaUrl);
+      const json = (await res.json());
+
+      const instaItem: InstaItem = {
+        permalink: json.permalink,
+        mediaUrl: json.media_url
+      };
+
+      return instaItem;
+    }
     const doFetch = async () => {
-      const res = await fetch(instaUrl, {
-        method: 'get',
-        headers: new Headers({
-          'Content-Type': 'application/json'
-        }),
-        redirect: "follow"
-      });
+      if(!userId || !accessToken) {
 
-      const text = await res.text();
-      const json = JSON.parse(text).data;
+        console.log("userId or accessToken is undefined: ", {userId, accessToken});
+        return;
+      }
+      const res = await fetch(instaUrl);
+      const json = (await res.json()).data;
+      console.log(json);
 
-      const loadedInstaItems: InstaItem[] = [];
+      const fetchedItems: InstaItem[] = [];
 
-      for (let i = 0; i < json.length && i < 9; i++) {
+      for(let i=0; i<json.length && i<9; i++) {
         const item = json[i];
-        const permalink = item.permalink;
-        const mediaUrl = item.media_url;
+        const itemId = item.id;
 
-        loadedInstaItems.push({permalink, mediaUrl});
-
-        console.log({ permalink, mediaUrl });
+        const instaItem = await fetchMedia(itemId);
+        fetchedItems.push(instaItem);
       }
 
-      setInstaItems(loadedInstaItems);
-    };
+      setInstaItems(fetchedItems);
+    }
 
     doFetch();
-
-  }, []);
+  }, [userId, accessToken, instaUrl]);
 
   return (
     <InstaGrid items={instaItems} />
