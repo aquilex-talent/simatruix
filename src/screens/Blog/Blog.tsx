461e6ed4f5cf7d5f56aabbed2f712d89a596e6e0
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-10-26 17:51:44 +0100

    Fix blog cards
    Previously the blog cards didn't actually open the articles. Now the cards open the articles in a new tab, use high quality images, and the code doesn't depend on any external APIs to do so

--- src/screens/Blog/Blog.tsx
+++ src/screens/Blog/Blog.tsx
@@ -11,67 +11,87 @@ export type Article = {
 const Blog: FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
 
+  const mediumUrl = "/feed/@fedor.selenskiy";
+
   useEffect(() => {
     const doFetch = async () => {
-      try{
-        const res = await fetch("/@fedor.selenskiy");
-        const html = await res.text();
-        
+      try {
+        const res = await fetch(mediumUrl);
+        const xml = await res.text();
         var node = new DOMParser().parseFromString(
-          html,
-          "text/html"
+          xml,
+          "text/xml"
         ).documentElement;
+        const articles = node.querySelectorAll("item");
 
-        const articles = node.querySelectorAll("article");
-
-        console.log("number of articles: ", articles.length);
-
-        const fetchedArticles: Article[] = [];
+        const loadedArticles: Article[] = [];
 
         for (let i = 0; i < articles.length; i++) {
-          const article = articles[i];
-
-          const title = article.querySelector("h2");
-          if(!title) {
+          const titleElement = articles[i].querySelector("title");
+          if (!titleElement || !titleElement.textContent) {
             continue;
           }
 
-          const urlElement = article.querySelector("a[aria-label='Post Preview Reading Time']");
-          if(!urlElement) {
+          const title = titleElement.textContent;
+
+          const linkElement = articles[i].querySelector("link");
+          if (!linkElement || !linkElement.textContent) {
             continue;
           }
 
-          const url = urlElement.getAttribute("href");
-          if(!url) {
+          const readMoreUrl = linkElement.textContent;
+
+          if (!articles[i]) {
             continue;
           }
 
-          const thisArticle: Article = {
-            title: title.innerText,
-            readMoreUrl: url
+          var encodedContent = articles[i].getElementsByTagNameNS(
+            "*",
+            "encoded"
+          );
+
+          if (!encodedContent || !encodedContent.item(0)) {
+            continue;
+          }
+
+          var content = encodedContent.item(0);
+          if (!content || !content.textContent) {
+            continue;
+          }
+
+          const parser = new DOMParser();
+          const parsedContent = parser.parseFromString(
+            content.textContent,
+            "text/html"
+          );
+
+          const img = parsedContent.querySelector("img");
+          if (!img) {
+            console.log("no img");
+            continue;
+          }
+
+          const imgUrl = img.getAttribute("src");
+          if (!imgUrl) {
+            console.log("no src");
+            continue;
+          }
+
+          const article: Article = {
+            imgUrl,
+            title,
+            readMoreUrl,
           };
 
-          console.log(thisArticle);
-
-          const img = article.querySelector("img");
-          if(!!img) {
-            thisArticle.imgUrl = img.src;
-          }
-
-          fetchedArticles.push(thisArticle);
-
+          loadedArticles.push(article);
         }
 
-        setArticles(fetchedArticles);
-
-      } catch(error) {
-        console.log("ERROR: ", error);
-      }
-      
-    }
+        setArticles(loadedArticles);
+      } catch (error) {}
+    };
 
     doFetch();
-  }, []);
+  }, [mediumUrl]);
 
   return <BlogList articles={articles} />;
 };
