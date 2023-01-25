e5dd7f4aa4c10af18dd16fb950cd6953f47d7f2c
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-01-25 13:59:04 +0000

    Clean up code

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -2,13 +2,11 @@ import React, { useEffect } from 'react';
 import { useSnapCarousel } from 'react-snap-carousel';
 
 const Carousel = () => {
-  const { scrollRef, pages, activePageIndex, next, prev, goTo } = useSnapCarousel();
+  const { scrollRef, pages, activePageIndex, next, goTo } = useSnapCarousel();
 
   useEffect(() => {
-    console.log('useEffect');
     setTimeout(() => {
-      console.log('inside setTimeout');
-      if(activePageIndex === pages.length - 1) {
+      if (activePageIndex === pages.length - 1) {
         goTo(0);
         return;
       }
@@ -18,34 +16,33 @@ const Carousel = () => {
   }, [activePageIndex, pages, goTo, next]);
 
   return (
-    <div style={{ width: '100%' }}>
-      <ul
-        ref={scrollRef}
-        style={{
-          display: 'flex',
-          overflow: 'hidden',
-          scrollSnapType: 'x mandatory'
-        }}
-      >
-        {Array.from({ length: 5 }).map((_, i) => (
-          <li
-            style={{
-              backgroundColor: 'aqua',
-              fontSize: '50px',
-              width: '100%',
-              height: '250px',
-              flexShrink: 0,
-              color: '#fff',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            Item {i}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul
+      ref={scrollRef}
+      style={{
+        width: '100%',
+        display: 'flex',
+        overflow: 'hidden',
+        scrollSnapType: 'x mandatory'
+      }}
+    >
+      {Array.from({ length: 5 }).map((_, i) => (
+        <li
+          style={{
+            backgroundColor: 'aqua',
+            fontSize: '50px',
+            width: '100%',
+            height: '250px',
+            flexShrink: 0,
+            color: '#fff',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          Item {i}
+        </li>
+      ))}
+    </ul>
   );
 };
 
